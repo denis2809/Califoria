04ff46d6c7ab228230eabdeae82fe389f9cf06e7
denis2809 <deniskucher2020@gmail.com>
2022-07-11 17:10:02 +0300

    corrections caruosel

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,10 @@
-// $(document).ready(function () {
-// 	$('.owl-carousel').owlCarousel({
-// 		loop: false,
-// 		nav: false,
-// 		dots: false,
-// 		// autoplay: true,
-// 		// autoplayTimeout: 3000,
-// 		margin: 0,
-// 		nav: true,
-// 		navText: ['', ' '],
-// 		responsive: {
-// 			0: {
-// 				items: 1
-// 			},
-// 		}
-// 	});
-// });
-
 $(document).ready(function () {
 
 	$('.owl-carousel').owlCarousel({
 		loop: true,
 		dots: false,
+		autoplay: true,
+		autoplayTimeout: 3000,
 		margin: 0,
 		nav: true,
 		navText: ['', ' '],
